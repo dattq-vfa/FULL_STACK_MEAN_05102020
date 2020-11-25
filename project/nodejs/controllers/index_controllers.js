65a8fe5ce59608d0ea96ff3fac4e7af72e0c7dfa
kientrumhodo <kientrumhodo997@gmail.com>
2020-11-25 21:11:55 +0700

    api

--- project/nodejs/controllers/index_controllers.js
+++ project/nodejs/controllers/index_controllers.js
@@ -12,4 +12,10 @@ router.use('/',product_controllers);
 const user_controllers = require('./user_controllers');
 router.use('/',user_controllers);
 
+const userApi_controllers = require('./userApi_controllers');
+router.use('/api/',userApi_controllers);
+
+const productApi_controllers = require('./productApi_controllers');
+router.use('/',productApi_controllers);
+
 module.exports = router; //xuat ra du lieu de su dung
