0227ad8d9e57ecbdcc01533fff3f678431dad816
kientrumhodo <kientrumhodo997@gmail.com>
2020-12-10 21:23:18 +0700

    post-service api

--- angular4122020/src/app/category/category.component.ts
+++ angular4122020/src/app/category/category.component.ts
@@ -4,7 +4,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { from } from 'rxjs';
 //gọi http, để gọi api
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+//gọi service
+import { CategoryService } from './category.service'
+import { ProductService } from '../product/product.service';
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -12,11 +16,10 @@ import {HttpClient} from '@angular/common/http';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor(private router:ActivatedRoute, private http: HttpClient) { }
+  constructor(private router:ActivatedRoute, private category: CategoryService) { }
 
   title;
-  obj;
-  api_url = 'http://localhost:3000/product/list';
+  // api_url = 'http://localhost:3000/product/list';
 
   ngOnInit(): void {
     this.router.paramMap.subscribe((param:ParamMap)=>{
@@ -24,9 +27,15 @@ export class CategoryComponent implements OnInit {
       this.title = param.get('id');
 
     });
-    this.http.get(this.api_url).subscribe((kq)=>{
-      this.obj = kq['data'];
+    //get
+    // this.http.get(this.api_url).subscribe((kq)=>{
+    //   this.obj = kq['data'];
+    // });
+
+    this.category.addcategory().subscribe((kq)=>{
+      console.log(kq);
     });
+
   }
 
 }
---
+++ angular4122020/src/app/category/category.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+  addcategory(){
+    let option = {
+      headers : new HttpHeaders().set("Content-Type","application/x-www-form-urlencoded")
+    }
+    let body = new URLSearchParams();
+    body.set("name","angular");
+    let url = "http://localhost:3000/product/add/list";
+    return this.http.post(url, body.toString(),option);
+  }
+}
